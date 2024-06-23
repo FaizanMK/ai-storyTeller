0223709fa41391d0d2bdc609dd0e591e1505d5e5
Faizan <muhammadfaizank119@gmail.com>
2024-06-23 10:31:51 +0500

    build story Writer UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
+import { BookOpen, FilePen } from "lucide-react";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header>
+    <header className="relative p-16 text-center">
       <Link href="/">
-        <h1>StoryTeller AI</h1>
-        <div className="flex space-x-5 text-3xl lg:text-5xl">
+        <h1 className="text-6xl font-bold">StoryTeller AI</h1>
+        <div className="flex justify-center whitespace-nowrap space-x-5 text-3xl lg:text-5xl">
           <h2>Bringing your stories</h2>
           <div className="relative">
             <div className="absolute bg-purple-500 -left-2 -top-1 -bottom-1 -right-2 md:-left-3 md:-top-0 md:-bottom-0 md:-right-3 -rotate-1" />
@@ -13,6 +14,14 @@ const Header = () => {
           </div>
         </div>
       </Link>
+      <div className=" absolute -top-5 right-5 flex space-x-2 ">
+        <Link href="/">
+          <FilePen className="h-8 w-8 lg:w-10 lg:h-10 mx-auto text-purple-500 mt-10 border border-purple-500 p-2 rounded-md hover:opacity-50 cursor-pointer" />
+        </Link>
+        <Link href="/stories">
+          <BookOpen className="h-8 w-8 lg:w-10 lg:h-10 mx-auto text-purple-500 mt-10 border border-purple-500 p-2 rounded-md hover:opacity-50 cursor-pointer" />
+        </Link>
+      </div>
     </header>
   );
 };
---
+++ components/StoryWriter.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { useState } from "react";
+
+const StoryWriter = () => {
+  const [story, setStory] = useState<string>("");
+  const [pages, setPages] = useState<number>();
+  const [progress, setProgress] = useState("");
+  const [runStarted, setRunStarted] = useState<boolean>(false);
+  const [runFinished, setRunFinished] = useState<boolean | null>(null);
+  const [currentTool, setcurrentTool] = useState("");
+  return (
+    <div className="flex flex-col container">
+      <section className="flex-1 flex flex-col border border-purple-300 rounded-md p-10 space-y-2">
+        <Textarea
+          value={story}
+          onChange={(e) => setStory(e.target.value)}
+          className="flex-1 text-black"
+          placeholder="Write a story about a human and a robot who become friends///"
+        />
+        <Select onValueChange={(value) => setPages(parseInt(value))}>
+          <SelectTrigger>
+            <SelectValue placeholder="How many pages should the story be?" />
+          </SelectTrigger>
+          <SelectContent className="w-full">
+            {Array.from({ length: 10 }, (_, i) => (
+              <SelectItem key={i} value={String(i + 1)}>
+                {i + 1}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <Button disabled={!story || !pages} className="w-full" size="lg">
+          Generate Story
+        </Button>
+      </section>
+      <section className="flex-1 pb-5 mt-5">
+        <div className="flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font-mono p-10 h-96 overflow-y-auto ">
+          <div>
+            {runFinished === null && (
+              <>
+                <p className="animate-pulse mr-5">
+                  I&apos;m waiting for you to generate a story above...
+                </p>
+                <br />
+              </>
+            )}
+            <span className="mr-5"> {">>"}</span>
+            {progress}
+          </div>
+          {/* Current Tool */}
+          {currentTool && (
+            <div>
+              <span className="py-10"> {"--- [Current Tool] ---"}</span>
+
+              {currentTool}
+            </div>
+          )}
+          {/* Render Events */}
+          {runStarted && (
+            <div>
+              <span className="mr-5 animate-in">
+                {"--- [AI StoryTeller Has Started] ---"}
+              </span>
+              <br />
+            </div>
+          )}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default StoryWriter;
