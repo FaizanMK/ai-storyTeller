466591a42893a2d1efe6bd5e6bc27af96727d664
Faizan <muhammadfaizank119@gmail.com>
2024-06-25 10:41:10 +0500

    server actions

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -10,14 +10,90 @@ import {
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { useState } from "react";
+import { Frame } from "@gptscript-ai/gptscript";
+import renderEventMessage from "@/lib/renderEventMessage";
 
+// this is where I want the GPT to go ahead and write the stories. (we don;t have the Database, we are writing to the server itself)
+const storiesPath = "public/stories";
 const StoryWriter = () => {
   const [story, setStory] = useState<string>("");
   const [pages, setPages] = useState<number>();
   const [progress, setProgress] = useState("");
   const [runStarted, setRunStarted] = useState<boolean>(false);
   const [runFinished, setRunFinished] = useState<boolean | null>(null);
-  const [currentTool, setcurrentTool] = useState("");
+  const [currentTool, setCurrentTool] = useState("");
+  const [events, setEvents] = useState<Frame[]>([]);
+
+  async function runScript() {
+    setRunStarted(true);
+    setRunFinished(false);
+
+    const response = await fetch("/api/run-script", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      //   story, pages and path will be the first 3 arguments of our GPT script
+      body: JSON.stringify({ story, pages, path: storiesPath }),
+    });
+
+    if (response.ok && response.body) {
+      // Handle streams from the API
+      // ...
+      console.log("Streaming started");
+      const reader = response.body.getReader();
+      const decoder = new TextDecoder();
+
+      handleStream(reader, decoder);
+    } else {
+      setRunFinished(true);
+      setRunStarted(false);
+      console.error("Failed to start streaming");
+    }
+  }
+
+  async function handleStream(
+    reader: ReadableStreamDefaultReader<Uint8Array>,
+    decoder: TextDecoder
+  ) {
+    // Manage the stream from the API ...
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break; //breaks out of the infinite loop
+      // Explanation: The decoder is used to decode the Uint8Array into a string.
+      const chunk = decoder.decode(value, { stream: true });
+
+      // Explanation: We split the chunk into events by splitting it by the event: keyword
+      const eventData = chunk
+        .split("\n\n")
+        .filter((line) => line.startsWith("event: "))
+        .map((line) => line.replace(/^event: /, ""));
+
+      // Explanation: We parse the JSON data and update the state accordingly
+      eventData.forEach((data) => {
+        try {
+          const parsedData = JSON.parse(data);
+          console.log("i am in eventData", parsedData);
+          if (parsedData.type === "callProgress") {
+            setProgress(
+              parsedData.output[parsedData.output.length - 1].content
+            );
+            setCurrentTool(parsedData.tool?.description || "");
+          } else if (parsedData.type === "callStart") {
+            setCurrentTool(parsedData.tool?.description || "");
+          } else if (parsedData.type === "runFinish") {
+            setRunFinished(true);
+            setRunStarted(false);
+            console.log("Run Finished");
+          } else {
+            setEvents((prevEvents) => [...prevEvents, parsedData]);
+          }
+        } catch (error) {
+          console.error("Failed to parse JSON", error);
+        }
+      });
+    }
+  }
   return (
     <div className="flex flex-col container">
       <section className="flex-1 flex flex-col border border-purple-300 rounded-md p-10 space-y-2">
@@ -39,7 +115,12 @@ const StoryWriter = () => {
             ))}
           </SelectContent>
         </Select>
-        <Button disabled={!story || !pages} className="w-full" size="lg">
+        <Button
+          disabled={!story || !pages || runStarted}
+          className="w-full"
+          size="lg"
+          onClick={runScript}
+        >
           Generate Story
         </Button>
       </section>
@@ -66,6 +147,14 @@ const StoryWriter = () => {
             </div>
           )}
           {/* Render Events */}
+          <div className="space-y-5">
+            {events.map((event, index) => (
+              <div key={index}>
+                <span className="mr-5">{">>"}</span>
+                {renderEventMessage(event)}
+              </div>
+            ))}
+          </div>
           {runStarted && (
             <div>
               <span className="mr-5 animate-in">
